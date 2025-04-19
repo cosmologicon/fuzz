5808f899d3c1aac2cc90949cf6da85046c70681b
Christopher Night <cosmologicon@gmail.com>
2025-04-19 09:40:38 -0400

    Rename API functions to match `random` module

--- fuzz.py
+++ fuzz.py
@@ -1,37 +1,39 @@
 
 # https://github.com/cosmologicon/fuzz
 
-import math, random, functools
+import math, functools
 
 CACHE_RESULTS = True
 
-def fuzz(*args):
+def hash(*args):
 	a = 0.12
-	a = random.random()
 	for x in args:
 		a += x + 3.45
 		a *= 0.678
-	return 91011.12 * math.sin(a) % 1
+	return a
+
+def random(*args):
+	return 91011.12 * math.sin(hash(*args)) % 1
 
 if CACHE_RESULTS:
-    fuzz = functools.lru_cache(1000000)(fuzz)
+    random = functools.lru_cache(1000000)(random)
 
-def fuzzflip(p, *args):
-    return fuzz(*args) < p
+def flip(p, *args):
+    return random(*args) < p
 
-def fuzzrange(a, b, *args):
-	return a + (b - a) * fuzz(*args)
+def uniform(a, b, *args):
+	return a + (b - a) * random(*args)
 
-def fuzzint(a, b, *args):
-	x = int(math.floor(fuzzrange(a, b + 1, *args)))
+def randint(a, b, *args):
+	x = int(math.floor(uniform(a, b + 1, *args)))
 	return min(max(x, a), b)
 
 def choice(values, *args):
-	return values[fuzzint(0, len(values) - 1, *args)]
+	return values[randint(0, len(values) - 1, *args)]
 
 def shuffle(values, *args):
 	for j in range(len(values) - 1):
-		k = fuzzint(j, len(values) - 1, j, *args)
+		k = randint(j, len(values) - 1, j, *args)
 		if j != k:
 			values[j], values[k] = values[k], values[j]
 	return values
